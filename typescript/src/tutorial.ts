bc10d2aa2df08926a88ba1e845f9817955117870
Giorgos Vontzalidis <gvontzalidis@hotmail.com>
2025-12-25 20:07:04 +0200

    tutorial

---
+++ typescript/src/tutorial.ts
@@ -0,0 +1,15 @@
+
+let awesomeName = 'george';
+awesomeName = 'something';
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName);
+
+// awesomeName = 20;
+
+let amount = 20;
+amount = 12-1;
+// amount = 'pants';
+
+let isAwesome = true;
+isAwesome = false;
+// isAwesome = 'something';
