eaaefe52903fff4f11b03fc81fc7b027879ceb67
Giorgos Vontzalidis <gvontzalidis@hotmail.com>
2025-12-28 00:20:55 +0100

    Update tutorial.ts

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -1,15 +1,2 @@
 
-let awesomeName = 'george';
-awesomeName = 'something';
-awesomeName = awesomeName.toUpperCase();
-console.log(awesomeName);
 
-// awesomeName = 20;
-
-let amount = 20;
-amount = 12-1;
-// amount = 'pants';
-
-let isAwesome = true;
-isAwesome = false;
-// isAwesome = 'something';
