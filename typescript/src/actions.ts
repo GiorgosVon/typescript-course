5051a3506ec3782a3763737cf92374727b546bfd
Giorgos Vontzalidis <gvontzalidis@hotmail.com>
2026-01-05 23:29:42 +0100

    update

---
+++ typescript/src/actions.ts
@@ -0,0 +1,19 @@
+
+export function sayHello (name:string): void {
+    console.log(`Hello ${name}`);
+    
+}
+
+export let person = 'susan';
+
+export type Student = {
+    name:string;
+    age:number;
+}
+
+const newStudent: Student = {
+    name:'peter',
+    age:24,
+};
+
+export default newStudent;
